a65668b1ac4f9571a1de6afcef32453de4952790
NicholasDJM <11604027+NicholasDJM@users.noreply.github.com>
2023-05-31 16:50:06 -0400

    Better maintainable Sources list

--- TFMOD/src/routes/main/sources/+page.svelte
+++ TFMOD/src/routes/main/sources/+page.svelte
@@ -5,58 +5,167 @@
 	import { currentPage } from "$lib/js/page.js";
 	currentPage.set("sources");
 	import Link from "$lib/components/Link.comp.svelte";
-	let translations: Record<string, string>;
+	type Sources = {
+		links: Array<{
+			translation: string
+			favicon?: string,
+			url: string,
+			alt: string
+		}>,
+		logo: {
+			url: string,
+			width: number,
+			height: number,
+			alt: string,
+			class?: string
+		},
+		url: string
+	}
+	let translations: Record<string, string> = {
+		// Must have default values, or we get an error.
+		tf2: "",
+		tf2c: "",
+		of: "",
+		pf2: "",
+		gamebanana: "",
+		mods: "",
+		grid: "",
+		gamebananaalt: "",
+		modsalt: "",
+		gridalt: ""
+	};
 	$: translations = {
 		tf2: $i18n.t("game-tf2"),
 		tf2c: $i18n.t("game-tf2c"),
 		of: $i18n.t("game-of"),
-		pf2: $i18n.t("game-pf2")
+		pf2: $i18n.t("game-pf2"),
+		gamebanana: $i18n.t("sources:gamebanana"),
+		mods: $i18n.t("sources:modstf"),
+		grid: $i18n.t("sources:grid"),
+		gamebananaalt: $i18n.t("sources:gamebanana-alt"),
+		modsalt: $i18n.t("sources:modstf-alt"),
+		gridalt: $i18n.t("sources:grid-alt")
 	};
+	let gb = {
+			translation: "gamebanana",
+			favicon: "https://icons.duckduckgo.com/ip2/gamebanana.com.ico",
+			alt: ""
+		},
+		modstf = {
+			translation: "mods",
+			favicon: "https://icons.duckduckgo.com/ip2/mods.tf.ico",
+			alt: ""
+		},
+		grid = {
+			translation: "grid",
+			favicon: "https://icons.duckduckgo.com/ip2/steamgriddb.com.ico",
+			alt: ""
+		},
+		source: Array<Sources>;
+	$: {
+		gb.alt = translations.gamebananaalt;
+		modstf.alt = translations.modsalt;
+		grid.alt = translations.gridalt;
+		source = [
+			{
+				links: [
+					{
+						...gb,
+						url: "https://tf2.gamebanana.com"
+					},
+					{
+						...modstf,
+						url: "https://mods.tf"
+					},
+					{
+						...grid,
+						url: "https://www.steamgriddb.com/game/10602"
+					}
+				],
+				logo: {
+					url: "/images/copyrighted_images/tf2.png",
+					alt: translations.tf2,
+					width: 512,
+					height: 128
+				},
+				url: "https://steampowered.com/app/440"
+			},
+			{
+				links: [
+					{
+						...gb,
+						url: "https://gamebanana.com/games/5418"
+					},
+					{
+						...grid,
+						url: "https://www.steamgriddb.com/game/5262654"
+					}
+				],
+				logo: {
+					url: "/images/copyrighted_images/tf2c.png",
+					alt: translations.tf2c,
+					width: 3000,
+					height: 959
+				},
+				url: "https://tf2classic.com"
+			},
+			{
+				links: [
+					{
+						...gb,
+						url: "https://gamebanana.com/games/7286"
+					},
+					{
+						...grid,
+						url: "https://www.steamgriddb.com/game/5248803"
+					}
+				],
+				logo: {
+					url: "/images/copyrighted_images/of.png",
+					alt: translations.of,
+					width: 2089,
+					height: 384,
+					class: "of-logo"
+				},
+				url: "https://openfortress.fun"
+			},
+			{
+				links: [
+					{
+						...gb,
+						url: "https://gamebanana.com/games/7748"
+					},
+					{
+						...grid,
+						url: "https://www.steamgriddb.com/game/5262280"
+					}
+				],
+				logo: {
+					url: "/images/copyrighted_images/pf2.webp",
+					alt: translations.pf2,
+					width: 1400,
+					height: 500
+				},
+				url: "https://prefortress.com"
+			}
+		];
+	}
 </script>
 <div class="defaultMargin main">
-	<div>
-		<Link href="https://steampowered.com/app/440">
-			<h1><img class="logo" alt={translations.tf2} src="/images/copyrighted_images/tf2.png" width="512" height="128"/></h1>
-		</Link>
-		<ul>
-			<li>
-				<Link href="https://tf2.gamebanana.com"><img alt="GameBanana Logo" src="https://icons.duckduckgo.com/ip2/gamebanana.com.ico" loading="lazy"/> GameBanana</Link>
-			</li>
-			<li>
-				<Link href="http://mods.tf"><img alt="GameBanana Logo" src="https://icons.duckduckgo.com/ip2/mods.tf.ico" loading="lazy"/> Mods.tf</Link>
-			</li>
-		</ul>
-	</div>
-	<div>
-		<Link href="https://tf2classic.com">
-			<h1><img class="logo" alt={translations.tf2c} src="/images/copyrighted_images/tf2c.png" width="3000" height="959"/></h1>
-		</Link>
-		<ul>
-			<li>
-				<Link href="https://www.gamebanana.com/games/5418"><img alt="GameBanana Logo" src="https://icons.duckduckgo.com/ip2/gamebanana.com.ico" loading="lazy"/> GameBanana</Link>
-			</li>
-		</ul>
-	</div>
-	<div>
-		<Link href="https://openfortress.fun">
-			<h1><img class="logo of-logo" alt={translations.of} src="/images/copyrighted_images/of.png" width="2089" height="384"/></h1>
-		</Link>
-		<ul>
-			<li>
-				<Link href="https://www.gamebanana.com/games/7286"><img alt="GameBanana Logo" src="https://icons.duckduckgo.com/ip2/gamebanana.com.ico" loading="lazy"/> GameBanana</Link>
-			</li>
-		</ul>
-	</div>
-	<div>
-		<Link href="https://prefortress.com">
-			<h1><img class="logo" alt={translations.pf2} src="/images/copyrighted_images/pf2.webp" width="1400" height="500"/></h1>
-		</Link>
-		<ul>
-			<li>
-				<Link href="https://www.gamebanana.com/games/7748"><img alt="GameBanana Logo" src="https://icons.duckduckgo.com/ip2/gamebanana.com.ico" loading="lazy"/> GameBanana</Link>
-			</li>
-		</ul>
-	</div>
+	{#each source as card}
+		<div>
+			<Link href={card.url}>
+				<h1><img class={card.logo.class + " logo"} src={card.logo.url} alt={card.logo.alt} width={card.logo.width} height={card.logo.height} loading="lazy"></h1>
+			</Link>
+			<ul>
+				{#each card.links as link}
+					<li>
+						<Link href={link.url}>{#if link.favicon}<img class="favicon" src={link.favicon} alt={link.alt} loading="lazy">{/if}{translations[link.translation]}</Link>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{/each}
 </div>
 <style lang="postcss">
 	@property --defaultMargin {
@@ -117,5 +226,17 @@
 	}
 	.of-logo {
 		filter: $ofDropShadow;
+	}
+	$gap: 0.45rem;
+	.favicon {
+		margin-inline-end: $gap;
+	}
+	li {
+		/* stylelint-disable-next-line rem-over-px/rem-over-px -- Size of favicon. */
+		margin-inline-start: calc(16px + $gap);
+	}
+	li:has(.favicon) {
+		marker-style: none;
+		margin-inline-start: 0;
 	}
 </style>
--- TFMOD/static/locales/en/sources.ftl
+++ TFMOD/static/locales/en/sources.ftl
@@ -1,1 +1,11 @@
 page-sources = Download New Mods
+
+-gb = GameBanana
+-m = Mods.tf
+-g = SteamGridDB
+gamebanana = {-gb}
+modstf = {-m}
+grid = {-g}
+gamebanana-alt = {-gb} Logo
+modstf-alt = {-m} Logo
+grid-alt = {-g} Logo"
